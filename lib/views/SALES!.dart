1a4b09539de8c6bf232b5a5e58e5c2d72fe36b77
Marios Giagkou <mariosyiangou99@gmail.com>
2025-11-22 16:49:12 +0000

    Add promotional content to SalesPage

--- lib/views/SALES!.dart
+++ lib/views/SALES!.dart
@@ -17,6 +17,44 @@ class SalesPage extends StatelessWidget {
               width: double.infinity,
               height: 300,
               color: Colors.white,
+              alignment: Alignment.topCenter,
+              padding: const EdgeInsets.only(top: 40),
+              child: const Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    'SALE',
+                    style: TextStyle(
+                      fontSize: 42,
+                      fontWeight: FontWeight.w700,
+                      letterSpacing: 1.2,
+                      color: Colors.black,
+                    ),
+                  ),
+                  SizedBox(height: 12),
+                  Text(
+                    'Don’t miss out! Get yours before they’re all gone!',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 16,
+                      height: 1.4,
+                      color: Color(0xFF555555),
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  SizedBox(height: 24),
+                  Text(
+                    'All prices shown are inclusive of the discount 🛒',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 14,
+                      height: 1.4,
+                      color: Color(0xFF444444),
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ],
+              ),
             ),
             const SizedBox(height: 40),
             const SiteFooter(),
@@ -26,4 +64,3 @@ class SalesPage extends StatelessWidget {
     );
   }
 }
- 
