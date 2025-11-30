a04d62fcd35addb5bdb0ed61e5fb6342a885d922
Marios Giagkou <mariosyiangou99@gmail.com>
2025-11-30 19:27:33 +0000

    Remove unused import for personalise_page in CollectionsPage

--- lib/views/collections_page.dart
+++ lib/views/collections_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:union_shop/models/layout.dart';
 import 'package:go_router/go_router.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:union_shop/views/personalise_page.dart';
 
 class CollectionsPage extends StatefulWidget {
   final String? category;
