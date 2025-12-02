d2a96caf0635572a25d81dd28e09dc8e69dbdf04
Marios Giagkou <mariosyiangou99@gmail.com>
2025-12-02 15:26:33 +0000

    Add comprehensive tests for SearchPage functionality and user interactions

--- test/views/search_page_tests/search_page_test_2.dart
+++ test/views/search_page_tests/search_page_test_2.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:union_shop/models/layout.dart';
 import 'package:union_shop/views/search_page.dart';
-import '../helpers/test_helpers.dart';
+import '../../helpers/test_helpers.dart';
 
 void main() {
   group('SearchPage (views)', () {
