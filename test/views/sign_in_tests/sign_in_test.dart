de76868a8a77a9ee61a69997bce9c99967de8357
Marios Giagkou <mariosyiangou99@gmail.com>
2025-12-02 15:25:28 +0000

    Refactor sign-in tests: move tests to a new file and enhance structure

--- test/views/sign_in_tests/sign_in_test.dart
+++ test/views/sign_in_tests/sign_in_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:union_shop/views/sign_in.dart';
-import '../helpers/test_helpers.dart';
+import '../../helpers/test_helpers.dart';
 
 void main() {
   group('SignInPage', () {
